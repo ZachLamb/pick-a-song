953df3d38f3d74e19407b1d4e9affdaebadc765d
zach.lamb <zach.lamb@regiscompany.com>
2020-06-12 17:06:36 -0700

    made card component and added in static text

--- noted/next.config.js
+++ noted/next.config.js
@@ -1,4 +1,19 @@
-const withSass = require('@zeit/next-sass')
-module.exports = withSass({
-  cssModules: true
-})
+const withImages = require('next-images');
+const withSass = require('@zeit/next-sass');
+const withCSS = require("@zeit/next-css");
+
+module.exports = withImages(withCSS(withSass({
+   webpack (config, options) {
+       config.module.rules.push({
+           test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+           use: {
+               loader: 'url-loader',
+               options: {
+                   limit: 100000
+               }
+           }
+       });
+
+       return config;
+   }
+})));
